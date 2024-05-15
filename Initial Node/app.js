6fca53bc5875e406aa77238c89e55b33830dbfeb
fabiotnagamine <nagamine.fabio.1998@gmail.com>
2024-05-14 22:25:29 -0300

    principios node

--- Initial Node/app.js
+++ Initial Node/app.js
@@ -1,5 +1,10 @@
-const mod1 = require('./mod1');
+// const mod1 = require('./mod1');
 
-console.log(mod1.nome, mod1.sobrenome);
+// console.log(mod1.nome, mod1.sobrenome);
 
-mod1.nomeCompleto();
+// mod1.nomeCompleto();
+
+
+const Pessoa = require('./mod1');
+const pessoa = new Pessoa('João', 'Silva');
+pessoa.nomeCompleto();
--- Initial Node/mod1.js
+++ Initial Node/mod1.js
@@ -1,12 +1,25 @@
-const nome = "Fabio";
-const sobrenome = "Nagamine";
+// const nome = "Fabio";
+// const sobrenome = "Nagamine";
 
-const nomeCompleto = () => {
-    console.log(nome, sobrenome);
+// const nomeCompleto = () => {
+//     console.log(nome, sobrenome);
+// }
+
+// module.exports.nome = nome;
+
+// module.exports.sobrenome = sobrenome;
+
+// module.exports.nomeCompleto = nomeCompleto;
+
+class Pessoa{
+    constructor(nome, sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    nomeCompleto(){
+        console.log(this.nome, this.sobrenome);
+    }
 }
 
-module.exports.nome = nome;
-
-module.exports.sobrenome = sobrenome;
-
-module.exports.nomeCompleto = nomeCompleto;
+module.exports = Pessoa;
