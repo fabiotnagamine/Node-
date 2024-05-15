bb1aedceb7b27eb59904c13617a317e806a32109
fabiotnagamine <nagamine.fabio.1998@gmail.com>
2024-05-14 21:24:23 -0300

    Initial Commit

---
+++ Initial Node/mod1.js
@@ -0,0 +1,10 @@
+const nome = "Fabio";
+const sobrenome = "Nagamine";
+
+const nomeCompleto = () => {
+    console.log(nome, sobrenome);
+}
+
+module.exports.nome = nome;
+
+module.exports.sobrenome = sobrenome;
