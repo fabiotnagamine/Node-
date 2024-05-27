e88c29176ef8a93eb3406ac702bfdaad86e4f8c6
fabiotnagamine <nagamine.fabio.1998@gmail.com>
2024-05-27 14:39:37 -0300

    chore: Add static file serving for public assets and update view template

--- Rotas/app.js
+++ Rotas/app.js
@@ -8,6 +8,8 @@ app.use(express.urlencoded({
     extended:true
 }));
 
+app.use(express.static(path.resolve('./src/public')));
+
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
