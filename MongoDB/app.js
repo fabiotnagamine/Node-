aa339945861c1f0c22e6e6be79ec2c83d153f220
fabiotnagamine <nagamine.fabio.1998@gmail.com>
2024-06-09 11:01:59 -0300

    feat: Add ejs and express dependencies, and update MongoDB connection in app.js

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -1,22 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const route = require('./routes');
-const path = require('path');
+const mongoose = require("mongoose");
+const connectionStrings = "mongodb+srv://takashi:Takashi1253@cluster0.gkdgt5u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(connectionStrings, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() =>{
+  console.log('OK')
+  app.emit('OK');
+});
 
 
-app.use(express.urlencoded({
-    extended:true
-}));
 
-app.use(express.static(path.resolve('./src/public')));
+const route = require("./routes");
+const path = require("path");
 
-app.set('views', path.resolve(__dirname, 'src', 'views'));
-app.set('view engine', 'ejs');
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static(path.resolve("./src/public")));
+
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
 
 app.use(route);
 
 app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000');
-    console.log('Servidor executando na porta 3000');
-  });
-
+  console.log("Acessar http://localhost:3000");
+  console.log("Servidor executando na porta 3000");
+});
